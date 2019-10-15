699d8833c9f5e484d70271cbb3476a1c8b4197fe
McKay <55812850+macabryson@users.noreply.github.com>
2019-10-14 18:26:23 -0600

    comment

--- GroupHW2_Try2/Controllers/HomeController.cs
+++ GroupHW2_Try2/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace GroupHW2_Try2.Controllers
+namespace GroupHW2_Try2.Controllers//testing comment
 {
     public class HomeController : Controller
     {
